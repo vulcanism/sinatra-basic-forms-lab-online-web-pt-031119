0239a8a6361af3b955d4aad640f8a04125b4fb06
Savannah C <sgcorrell@gmail.com>
2019-06-14 18:15:36 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
   
-  post "/puppy" do
+  post "/new" do
     @name = params[:name]
     @breed = params[:breed]
     @months_old = params[:months_old]
