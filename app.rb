48d955bf6d0f038dba2143ae1493d8d34196b98e
Savannah C <sgcorrell@gmail.com>
2019-06-14 18:16:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,22 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get "/" do
+    erb :index
+  end
+  
+  get "/new" do
+    erb :create_puppy
+  end
+  
+  post "/new" do
+    @name = params[:name]
+    @breed = params[:breed]
+    @months_old = params[:months_old]
+    puppy = Puppy.new(@name, @breed, @months_old)
+    
+    erb :display_puppy
+  end
 
 end
--- spec/sinatra_basic_forms_lab_spec.rb
+++ spec/sinatra_basic_forms_lab_spec.rb
@@ -35,7 +35,7 @@ describe App do
 
       expect(page).to have_field(:name)
       expect(page).to have_field(:breed)
-      expect(page).to have_field(:age)
+      expect(page).to have_field(:months_old)
     end
   end
 
@@ -46,7 +46,7 @@ describe App do
 
       fill_in(:name, :with => "Butch")
       fill_in(:breed, :with => "Mastiff")
-      fill_in(:age, :with => "6")
+      fill_in(:months_old, :with => "6")
 
       #the below css will match any element (input or button)
       #with a type attribute set to submit
@@ -58,7 +58,7 @@ describe App do
     end
 
     it "recieves a request from /new at /puppy" do
-      expect(page.current_path).to eq("/puppy")
+      expect(page.current_path).to eq("/new")
     end
 
     it "displays the puppy information dynamically" do
@@ -72,7 +72,7 @@ describe App do
 
       fill_in(:name, :with => "Byron")
       fill_in(:breed, :with => "Poodle")
-      fill_in(:age, :with => "9")
+      fill_in(:months_old, :with => "9")
 
       #the below css will match any element (input or button)
       #with a type attribute set to submit
